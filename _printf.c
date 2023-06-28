c3ab830bd6120781a70cd58997a1aaab7d120263
Peter Abolude <aboludepeter@gmail.com>
2023-06-28 09:08:06 +0000

    betty check

--- _printf.c
+++ _printf.c
@@ -2,6 +2,37 @@
 #include "main.h"
 
 /**
+ * print_character - Prints a single character and updates the count
+ * @ch: The character to print
+ * @count: Pointer to the count of characters printed
+ */
+void print_character(char ch, int *count)
+{
+_putchar(ch);
+(*count)++;
+}
+
+/**
+ * print_string - Prints a string and updates the count
+ * @str: The string to print
+ * @count: Pointer to the count of characters printed
+ */
+void print_string(char *str, int *count)
+{
+int i = 0;
+
+if (str == NULL)
+str = "(null)";
+
+while (str[i])
+{
+_putchar(str[i]);
+(*count)++;
+i++;
+}
+}
+
+/**
  * _printf - Produces output according to a format.
  * @format: The format string
  *
@@ -10,71 +41,44 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+va_list args;
+int count = 0;
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (format && *format)
-    {
-        if (*format == '%')
-        {
-            format++;
+while (format && *format)
+{
+if (*format == '%')
+{
+format++;
 
-            if (*format == '%')
-            {
-                _putchar('%');
-                count++;
-            }
-            else if (*format == 'c')
-            {
-                char ch = va_arg(args, int);
-                _putchar(ch);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                int i = 0;
+if (*format == '%')
+{
+print_character('%', &count);
+}
+else if (*format == 'c')
+{
+char ch = va_arg(args, int);
+print_character(ch, &count);
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+print_string(str, &count);
+}
+else
+{
+print_character('%', &count);
+print_character(*format, &count);
+}
+}
+else
+{
+print_character(*format, &count);
+}
 
-                if (str == NULL)
-                {
-                    char *null_str = "(null)";
-                    while (null_str[i])
-                    {
-                        _putchar(null_str[i]);
-                        count++;
-                        i++;
-                    }
-                }
-                else
-                {
-                    while (str[i])
-                    {
-                        _putchar(str[i]);
-                        count++;
-                        i++;
-                    }
-                }
-            }
-            else
-            {
-                _putchar('%');
-                count++;
-                _putchar(*format);
-                count++;
-            }
-        }
-        else
-        {
-            _putchar(*format);
-            count++;
-        }
-
-        format++;
-    }
-
-    va_end(args);
-
-    return (count);
+format++;
+}
+va_end(args);
+return (count);
 }
