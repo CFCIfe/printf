b672de43215c8c32c348a58258ed9797a5c19ed2
Peter Abolude <aboludepeter@gmail.com>
2023-06-28 09:05:00 +0000

    Updated to meet use case

--- _printf.c
+++ _printf.c
@@ -10,51 +10,71 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+    va_list args;
+    int count = 0;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format && *format)
-	{
-		if (*format == '%')
-		{
-			format++;
+    while (format && *format)
+    {
+        if (*format == '%')
+        {
+            format++;
 
-			if (*format == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-			else if (*format == 'c')
-			{
-				char ch = va_arg(args, int);
-				_putchar(ch);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
-				int i = 0;
+            if (*format == '%')
+            {
+                _putchar('%');
+                count++;
+            }
+            else if (*format == 'c')
+            {
+                char ch = va_arg(args, int);
+                _putchar(ch);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                int i = 0;
 
-				while (str[i])
-				{
-					_putchar(str[i]);
-					count++;
-					i++;
-				}
-			}
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
-		}
+                if (str == NULL)
+                {
+                    char *null_str = "(null)";
+                    while (null_str[i])
+                    {
+                        _putchar(null_str[i]);
+                        count++;
+                        i++;
+                    }
+                }
+                else
+                {
+                    while (str[i])
+                    {
+                        _putchar(str[i]);
+                        count++;
+                        i++;
+                    }
+                }
+            }
+            else
+            {
+                _putchar('%');
+                count++;
+                _putchar(*format);
+                count++;
+            }
+        }
+        else
+        {
+            _putchar(*format);
+            count++;
+        }
 
-		format++;
-	}
+        format++;
+    }
 
-	va_end(args);
+    va_end(args);
 
-	return (count);
+    return (count);
 }
