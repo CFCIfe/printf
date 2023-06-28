72f0a28d9a9376bf25adbd91513ec37951586a29
Peter Abolude <aboludepeter@gmail.com>
2023-06-28 07:17:36 +0000

    Refreshed _printf.c

--- _printf.c
+++ _printf.c
@@ -1,44 +1,60 @@
+#include <stdarg.h>
 #include "main.h"
 
 /**
- * _printf - Custom printf function
- * @format: Format string
+ * _printf - Produces output according to a format.
+ * @format: The format string
  *
- * Return: Number of characters printed
+ * Return: The number of characters printed (excluding the null byte used to
+ * end output to strings)
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+	va_list args;
+	int count = 0;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            _putchar(*format);
-            count++;
-        }
-        else
-        {
-            format++;
-            if (*format == '\0')
-                break;
-            if (*format == '%')
-            {
-                _putchar('%');
-                count++;
-            }
-            else if (*format == 's')
-            {
-                count += print_string(args);
-            }
-        }
-        format++;
-    }
+	while (format && *format)
+	{
+		if (*format == '%')
+		{
+			format++;
 
-    va_end(args);
+			if (*format == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else if (*format == 'c')
+			{
+				char ch = va_arg(args, int);
+				_putchar(ch);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+				int i = 0;
 
-    return count;
+				while (str[i])
+				{
+					_putchar(str[i]);
+					count++;
+					i++;
+				}
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+
+		format++;
+	}
+
+	va_end(args);
+
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,15 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
-#include <stdarg.h>
-
-int _putchar(char c);
 int _printf(const char *format, ...);
-int print_decimal(int n);
-int print_integer(int n);
-int print_string(va_list args);
-int print_char(va_list args);
-int print_percent(va_list args);
+int _putchar(char c);
 
 #endif /* MAIN_H */
